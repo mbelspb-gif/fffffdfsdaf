7d90e2db2907029d47ef53acdb46e4f812228543
mbelspb-gif <mbelspb@gmail.com>
2026-04-28 07:34:42 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -6,57 +6,13 @@
 
 #subscription-userinfo: upload=0; download=0; total=0; expire=25555555560576000
 
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@178.248.238.85:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=ads.x5.ru&fp=chrome&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇳🇱 1X - Моб. интернет 
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@185.231.101.210:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=auto.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#💎 Авто | WI-FI⚡⚡️⚡️ 
+vless://ac53d0e8-23b7-4ea3-ae53-e58c69d16014@mss.mkaservice.top:8443?security=reality&encryption=none&echfq=none&pbk=i3F_hP7yc9VpcUC2Vn1oAqLyW3hZayGdTybl-VnWdz0&headerType=none&fp=chrome&allowinsecure=0&type=tcp&flow=xtls-rprx-vision&sni=www.vk.com&sid=02827ef87f4a23c0#%F0%9F%87%B7%F0%9F%87%BA%D0%91%D0%B5%D0%BB%D1%8B%D0%B9+%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA%F0%9F%8F%B3%EF%B8%8F+%5B1%5D
+trojan://zGEuozvK6JLdcpfkZl09kw@ru3.connectservice.top:41458?security=reality&pbk=i3F_hP7yc9VpcUC2Vn1oAqLyW3hZayGdTybl-VnWdz0&headerType=none&fp=chrome&allowinsecure=0&type=tcp&sni=max.ru&sid=02827ef87f4a23c0#%F0%9F%87%B7%F0%9F%87%BA+%D0%91%D0%B5%D0%BB%D1%8B%D0%B9+%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA+%F0%9F%8F%B3%EF%B8%8F+%5B2%5D
+vless://ac53d0e8-23b7-4ea3-ae53-e58c69d16014@ru2.connectservice.top:34443?security=reality&encryption=none&echfq=none&pbk=i3F_hP7yc9VpcUC2Vn1oAqLyW3hZayGdTybl-VnWdz0&headerType=none&fp=chrome&allowinsecure=0&type=tcp&flow=xtls-rprx-vision&sni=api-maps.yandex.ru&sid=02827ef87f4a23c0#%F0%9F%87%AA%F0%9F%87%BA+%D0%91%D0%B5%D0%BB%D1%8B%D0%B9+%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA+%F0%9F%8F%B3%EF%B8%8F+%5B3%5D
 
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@89.105.212.140:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=auto.quattro-tech.ru&fp=edge&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#⬇️ Быстрые | до 10 Гбит/с ⬇️
+vless://41bc01e9-6511-4b1e-96c3-d0d55990b343@149.33.23.248:443?security=reality&encryption=none&echfq=none&pbk=nPcUKydxoRI66O3tT9O4QCZpLjOBvkGsiXG7pDX1BBw&headerType=none&fp=chrome&allowinsecure=0&type=tcp&flow=xtls-rprx-vision&sni=www.google.com&sid=6ba85179e30d4fc2#%F0%9F%87%B2%F0%9F%87%BD+%D0%9C%D0%B5%D0%BA%D1%81%D0%B8%D0%BA%D0%B0
+vless://41bc01e9-6511-4b1e-96c3-d0d55990b343@149.33.23.248:8443?mode=auto&path=%2Fxhttp&security=reality&encryption=none&echfq=none&pbk=nPcUKydxoRI66O3tT9O4QCZpLjOBvkGsiXG7pDX1BBw&host=www.google.com&fp=chrome&allowinsecure=0&type=xhttp&sni=www.google.com&sid=6ba85179e30d4fc2#%F0%9F%87%B2%F0%9F%87%BD+%D0%9C%D0%B5%D0%BA%D1%81%D0%B8%D0%BA%D0%B0+%232
+vless://41bc01e9-6511-4b1e-96c3-d0d55990b343@64.188.82.86:443?security=reality&encryption=none&echfq=none&pbk=nPcUKydxoRI66O3tT9O4QCZpLjOBvkGsiXG7pDX1BBw&headerType=none&fp=chrome&allowinsecure=0&type=tcp&flow=xtls-rprx-vision&sni=www.google.com&sid=6ba85179e30d4fc2#%F0%9F%87%AA%F0%9F%87%AA+%D0%AD%D1%81%D1%82%D0%BE%D0%BD%D0%B8%D1%8F
+vless://41bc01e9-6511-4b1e-96c3-d0d55990b343@fn.e0f.network:443?security=reality&encryption=none&echfq=none&pbk=nPcUKydxoRI66O3tT9O4QCZpLjOBvkGsiXG7pDX1BBw&headerType=none&fp=chrome&allowinsecure=0&type=tcp&flow=xtls-rprx-vision&sni=www.google.com&sid=6ba85179e30d4fc2#%F0%9F%87%AB%F0%9F%87%AE+%D0%A4%D0%B8%D0%BD%D0%BB%D1%8F%D0%BD%D0%B4%D0%B8%D1%8F
+vless://41bc01e9-6511-4b1e-96c3-d0d55990b343@de.e0f.network:443?security=reality&encryption=none&echfq=none&pbk=nPcUKydxoRI66O3tT9O4QCZpLjOBvkGsiXG7pDX1BBw&headerType=none&fp=chrome&allowinsecure=0&type=tcp&flow=xtls-rprx-vision&sni=www.google.com&sid=6ba85179e30d4fc2#%F0%9F%87%A9%F0%9F%87%AA+%D0%93%D0%B5%D1%80%D0%BC%D0%B0%D0%BD%D0%B8%D1%8F
 
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@128.127.181.15:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=be.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇧🇪 ⭐️ Бельгия
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@uk.quattro-tech.ru:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=uk.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇬🇧 ⚡️ Великобритания
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@50.7.156.34:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=de.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇩🇪 ⚡️ Германия
-
-vless://ed3f447e-1b2b-0044-a797-151ab1949b93@50.7.4.66:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=auto.quattro-tech.ru&fp=chrome&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇮🇪 ⭐️ Ирландия
-
-vless://ed3f447e-1b2b-0006-a797-151ab1949b93@cloud.quattro-tech.ru:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=cloud.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇪🇸 ⚡️ Испания
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@158.58.172.126:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=auto.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇮🇹 ⚡️ Италия
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@46.183.223.210:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=lv.quattro-tech.ru&fp=edge&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇱🇻 ⚡️ Латвия
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@95.164.91.94:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=lv.quattro-tech.ru&fp=edge&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇱🇻 ⚡️ Латвия #2
-
-vless://ed3f447e-1b2b-0009-a797-151ab1949b93@cloud.quattro-tech.ru:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=cloud.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇱🇹 ⭐️ Литва
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@192.241.120.22:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=nl.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇳🇱 ⚡️ Нидерланды | Torrent ✅
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@89.105.212.138:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=nl.quattro-tech.ru&fp=edge&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇳🇱 ⚡️ Нидерланды #2 | Torrent ✅
-
-vless://ed3f447e-1b2b-000c-a797-151ab1949b93@cloud.quattro-tech.ru:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=cloud.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇳🇴 ⭐️ Норвегия
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@144.31.2.184:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=pl.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇵🇱 ⭐️ Польша
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@176.108.241.192:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=cloud.quattro-tech.ru&fp=edge&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇷🇺 Россия, Москва
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@212.193.31.180:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=auto.quattro-tech.ru&fp=edge&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇷🇺 Россия, Санкт-Петербург
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@80.87.102.108:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=auto.quattro-tech.ru&fp=edge&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇷🇺 ⚡ Россия, Екатеринбург
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@66.90.92.50:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=auto.quattro-tech.ru&fp=edge&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇷🇺 ⚡ Россия, Хабаровск
-
-vless://ed3f447e-1b2b-000e-a797-151ab1949b93@cloud.quattro-tech.ru:8443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=cloud.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇷🇴 ⭐️ Румыния
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@85.192.49.141:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=fi.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇫🇮 ⭐️ Финляндия
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@198.16.113.170:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=fr.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇫🇷 ⚡️ Франция
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@ch.quattro-tech.ru:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=ch.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇨🇭 ⚡️ Швейцария
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@195.47.238.14:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=se.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇸🇪 ⚡️ Швеция
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@185.215.184.74:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=ee.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇪🇪 ⚡️ Эстония
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@185.223.171.7:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=ee.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇪🇪 ⚡️ Эстония #2
-
-vless://ed3f447e-1b2b-4d16-a797-151ab1949b93@91.235.234.40:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=reality&sni=ee.quattro-tech.ru&fp=qq&pbk=10rVZPoOUP1TlQviIAsQ_jAROX0fRQxH0C92nq_zGQc&sid=43dcff53849b81e6#🇪🇪 ⚡️ Эстония #3
